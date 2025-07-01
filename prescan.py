426c083ed208c0b71b0b757c67925be73f0c09dd
Coreypdumais <Coreypdumais@gmail.com>
2025-07-01 18:33:56 -0400

    added notes to better track

--- prescan.py
+++ prescan.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 print("hello world")
 
-df1 = pd.read_csv('enriched_collection.csv')
-df2 = pd.read_csv(r"C:\Users\Corey\Downloads\ManaBox_Collection71.csv")
+df1 = pd.read_csv('enriched_collection.csv') # old collection here
+df2 = pd.read_csv(r"C:\Users\Corey\Downloads\ManaBox_Collection71.csv") # new collection here
 
 merged_df = pd.merge(df1, df2, on='Scryfall ID', how='inner', suffixes=('_df1', '_df2'))
 
 unique_to_df2 = df2[~df2['Scryfall ID'].isin(df1['Scryfall ID'])]
 
-unique_to_df2.to_csv('new_uploads.csv', index=False)
+unique_to_df2.to_csv('new_uploads.csv', index=False) # this is the file to run in main file
 
 
 print(len(df1))
