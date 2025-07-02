5c3ee1f89d116ae73ddecab52d092e5e6202df75
Coreypdumais <Coreypdumais@gmail.com>
2025-07-01 22:13:46 -0400

    added new and old status

--- postscan.py
+++ postscan.py
@@ -3,7 +3,12 @@ import pandas as pd
 df1 = pd.read_csv('enriched_collection.csv') # current full collection
 df2 = pd.read_csv('new_uploads.csv') # new uploads from prescan
 
+df1['Status'] = 'Old'
+df2['Status'] = 'Newly Added'
+
 combined_df = pd.concat([df1, df2], ignore_index=True)
+
+print(combined_df.head(25))
 
 print(len(combined_df)) # this is a check
 
